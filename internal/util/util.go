5c495b162a63452ac4e26bb449e3484a6a86cc6d
Erfan Momeni <Erfan.Momeni@snapp.cab>
2023-03-23 16:16:05 +0430

    fix: change separator

--- internal/util/util.go
+++ internal/util/util.go
@@ -6,7 +6,7 @@ func GenerateHeader(headers []string) (hs map[string][]string) {
 	for _, header := range headers {
 		i := strings.Index(header, ":")
 		if i != -1 {
-			hs[header[:i]] = strings.Split(header[i+1:], ";")
+			hs[header[:i]] = strings.Split(header[i+1:], ",")
 		}
 	}
 
